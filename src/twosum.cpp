bf271459ff978f02b7698cb9e032dd5e988e1af6
Tomoka Iwase <tomoka1227tw@gmail.com>
2026-05-09 22:18:50 +0800

    add test_twosum.cpp and ci.yml (#3)
    * add test_twosum.cpp and ci.yml
    
    * update ci.yml to use spaces insted of tabs
    
    * Update tests to use std::size_t

--- src/twosum.cpp
+++ src/twosum.cpp
@@ -10,10 +10,10 @@
  * @return A vector containing the two indices, or an empty vector if no solution is found.
  */
 std::vector<int> twoSumBruteForce(const std::vector<int>& nums, int target) {
-    for (size_t i = 0; i < nums.size(); ++i) {
-        for (size_t j = i + 1; j < nums.size(); ++j) {
+    for (std::size_t i = 0; i < nums.size(); ++i) {
+        for (std::size_t j = i + 1; j < nums.size(); ++j) {
             if (nums[i] + nums[j] == target) {
-                return {(int)i, (int)j};
+                return std::vector<int>{(int)i, (int)j};
             }
         }
     }
@@ -28,10 +28,10 @@ std::vector<int> twoSumBruteForce(const std::vector<int>& nums, int target) {
  */
 std::vector<int> twoSumOptimized(const std::vector<int>& nums, int target) {
     std::unordered_map<int, int> num_map;
-    for (size_t i = 0; i < nums.size(); ++i) {
+    for (std::size_t i = 0; i < nums.size(); ++i) {
         int complement = target - nums[i];
         if (num_map.count(complement)) {
-            return {num_map[complement], (int)i};
+            return std::vector<int>{num_map[complement], (int)i};
         }
         num_map[nums[i]] = (int)i;
     }
